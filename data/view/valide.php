e3dd972a370f654a739158a86b7fc98af081c3a5
Alexis Ferrand <aleferra@z1r10p1.42lyon.fr>
2024-06-04 15:01:19 +0200

    succes add

--- data/view/valide.php
+++ data/view/valide.php
@@ -1,11 +1,11 @@
 <?php
-$to = 'camagruweb@gmail.com';
+$to = 'a.ferrand69@gmail.com';
 $subject = 'Test d\'e-mail depuis Docker';
 $message = 'Ceci est un test d\'e-mail envoyé depuis Docker.';
-$headers = 'From: camagruweb@gmail.com';
+$headers = 'From: doudoufoot.em@live.com';
 
 // Commande pour envoyer l'e-mail via msmtp
-$command = 'echo "' . $message . '" | msmtp -a alex ' . $to . ' 2>&1';
+$command = 'echo "' . $message . '" | msmtp -a camagru ' . $to . ' 2>&1';
 
 // Exécute la commande et capture la sortie
 $output = shell_exec($command);
@@ -13,3 +13,4 @@ $output = shell_exec($command);
 // Affiche la sortie (y compris les éventuelles erreurs)
 echo nl2br($output);
 ?>
+<!-- echo -e "Subject: Test\n\nThis is a test email." | msmtp --debug --from=default -t doudoufoot.em@live.fr -->
